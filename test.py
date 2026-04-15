8bb734d764de405b2f59c04c0cc44856dd857c4c
udayreddy368-gif <udayreddy368@gmail.com>
2026-04-15 06:29:07 +0000

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://zuplay.com/")
+print(driver.title)
+driver.quit()
