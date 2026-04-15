c8546baa0c39ed53f904fd69912974e7e71e52ee
udayreddy368-gif <udayreddy368@gmail.com>
2026-04-15 06:42:17 +0000

    py

--- test.py
+++ test.py
@@ -2,13 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+import time
 
 options = Options()
-options.add_argument("--headless")
+
+# Headless mode with better camouflage
+options.add_argument("--headless=new")          # Newer headless mode (less detectable)
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-blink-features=AutomationControlled")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# Spoof a common user agent
+options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+
+# Additional evasion flags
+options.add_argument("--disable-web-security")
+options.add_argument("--allow-running-insecure-content")
+options.add_argument("--disable-features=IsolateOrigins,site-per-process")
+options.add_argument("--disable-site-isolation-trials")
+
+# Exclude automation switches
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+
+# Launch browser
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+# Execute CDP command to hide webdriver property
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": """
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined
+        });
+        Object.defineProperty(navigator, 'plugins', {
+            get: () => [1, 2, 3, 4, 5]
+        });
+        Object.defineProperty(navigator, 'languages', {
+            get: () => ['en-US', 'en']
+        });
+    """
+})
+
+# Navigate to site
 driver.get("https://zuplay.com/")
-print(driver.title)
+time.sleep(3)  # Allow redirects/JS to settle
+print("Page title:", driver.title)
+print("Current URL:", driver.current_url)
+
 driver.quit()
